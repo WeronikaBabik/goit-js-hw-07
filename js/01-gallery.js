ebd232d760c7fd4ab658b0263576ff52c1747197
VeranikaBabik <veranikababik@gmail.com>
2023-04-25 14:25:46 +0200

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const ul = document.querySelector(".gallery");
+galleryItems.forEach((image) => {
+  const item = document.createElement("li");
+  item.innerHTML = `<div class="gallery__item">
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</div>`;
+  ul.append(item);
+});
+ul.addEventListener("click", selectImage);
+function selectImage(event) {
+  event.preventDefault();
+  if (event.target.tagName !== "IMG") {
+    return;
+  }
+  ul.onclick = () => {
+    const imageSrc = event.target.dataset.source;
+    basicLightbox
+      .create(`<img width="1400" height="900" src="${imageSrc}">`)
+      .show();
+  };
+}
