160d24daa97a0c6b207dac84e64857db11d64316
VeranikaBabik <veranikababik@gmail.com>
2023-04-25 15:45:47 +0200

    task 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,4 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 
 const ul = document.querySelector(".gallery");
 galleryItems.forEach((image) => {
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
+const ul = document.querySelector(".gallery");
+galleryItems.forEach((image) => {
+  const item = document.createElement("li");
+  item.innerHTML = `<a class="gallery__item" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      alt="${image.description}"
+    />
+  </a>`;
+  ul.append(item);
+});
+ul.addEventListener("click", selectImage);
+function selectImage(event) {
+  event.preventDefault();
+  if (event.target.tagName !== "IMG") {
+    return;
+  }
+  let gallery = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: "250",
+  });
+}
